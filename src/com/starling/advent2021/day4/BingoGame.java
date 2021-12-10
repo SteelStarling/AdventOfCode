6a2a77e70804d1b3bd662b5bf22f93b305cffc2e
SteelStarling <58285541+SteelStarling@users.noreply.github.com>
2021-12-09 18:47:18 -0700

    Completed Day 4 Part 2 and changed formatting of Day 4 Part 1 to match with Part 2's styling.

--- src/com/starling/advent2021/day4/BingoGame.java
+++ src/com/starling/advent2021/day4/BingoGame.java
@@ -51,17 +51,49 @@ public class BingoGame {
     }
 
     /**
-     * Function: makeMoves
+     * Function: getWinningCard
      * Purpose:  Makes all moves until a winning card is found, then returns its score
      * @return  an int with the score of the winning card, or -1 if no card won
      */
-    public int makeMoves() {
+    public int getWinningCard() {
         for(int i = 0; i < callList.length; i++) {
             int call = callList[i];
             for(BingoCard card : cardList) {
                 //mark card and return if won
                 if(card.markCard(call)) {
                     return card.getScore(call);
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Function: getLosingCard
+     * Purpose:  Makes all moves until the last card that hasn't won is found, then returns its score
+     * @return  an int with the score of the winning card, or -1 if no card won
+     */
+    public int getLosingCard() {
+        boolean[] hasWon = new boolean[cardList.length];
+        for(int i = 0; i < callList.length; i++) {
+            int call = callList[i];
+            for(int j = 0; j < cardList.length; j++) {
+                //mark card and return if won
+                if(!hasWon[j] && cardList[j].markCard(call)) {
+                    hasWon[j] = true;
+                }
+
+                //check if all cards have won (card j must be last to win)
+                boolean allWon = true;
+                for(int k = 0; k < cardList.length && allWon; k++) {
+                    if(!hasWon[k]) {
+                        allWon = false;
+                    }
+                }
+
+                if(allWon) {
+                    return cardList[j].getScore(call);
                 }
             }
         }
