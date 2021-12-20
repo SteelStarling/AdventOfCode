60aab3f09a715e7a65ab3c8ef6effb7d04a9f014
IridiumIguana <71394260+IridiumIguana@users.noreply.github.com>
2021-12-20 00:15:45 -0700

    Added and implemented D5P2 and added implementation for 45 degree diagonals to be accounted for.

--- src/com/starling/advent2021/day5/Line.java
+++ src/com/starling/advent2021/day5/Line.java
@@ -6,6 +6,8 @@ public class Line {
     private Point p2;
     private boolean isVert;
     private boolean isHori;
+    private boolean isDiagD;
+    private boolean isDiagU;
 
     public Line(String input) {
         // calculate points
@@ -16,6 +18,10 @@ public class Line {
         // calculate if straight line
         this.isVert = this.calcVert();
         this.isHori = this.calcHori();
+
+        // calculate if exactly diagonal
+        this.isDiagD = this.calcDiagD();
+        this.isDiagU = this.calcDiagU();
     }
 
     public Line(int x1, int y1, int x2, int y2) {
@@ -30,6 +36,10 @@ public class Line {
         // calculate if straight line
         this.isVert = this.calcVert();
         this.isHori = this.calcHori();
+
+        // calculate if exactly diagonal
+        this.isDiagD = this.calcDiagD();
+        this.isDiagU = this.calcDiagU();
     }
 
     public Point getP1() {
@@ -52,12 +62,36 @@ public class Line {
         return this.isVert || this.isHori;
     }
 
+    public boolean isDiagD() {
+        return isDiagD;
+    }
+
+    public boolean isDiagU() {
+        return isDiagU;
+    }
+
+    public boolean isDiag() {
+        return this.isDiagU || this.isDiagD;
+    }
+
+    public boolean isUsable() {
+        return this.isStraight() || this.isDiag();
+    }
+
     public boolean calcVert() {
         return p1.getX() == p2.getX();
     }
 
     public boolean calcHori() {
         return p1.getY() == p2.getY();
+    }
+
+    public boolean calcDiagU() {
+        return (p1.getX() - p2.getX()) == (p1.getY() - p2.getY());
+    }
+
+    public boolean calcDiagD() {
+        return -(p1.getX() - p2.getX()) == (p1.getY() - p2.getY());
     }
 
     public String toString() {
