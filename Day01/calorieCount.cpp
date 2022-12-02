780a68cb6901366d5ba503ab28e7c1658ccd6654
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2022-12-01 17:21:15 -0700

    Initial Commit

---
+++ Day01/calorieCount.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Taylor Hancock on 12/01/22.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Day01/
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
