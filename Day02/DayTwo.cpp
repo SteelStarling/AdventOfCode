257c9816e1b2cac8d2cb5ca3ef52b5f405c9990a
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2022-12-02 21:45:53 -0700

    Completed Part Two

--- Day02/DayTwo.cpp
+++ Day02/DayTwo.cpp
@@ -15,6 +15,11 @@ void DayTwo::test() {
     solvePartOne("exampleInput.txt");
     cout << endl << "Input: ";
     solvePartOne("input.txt");
+    cout << "---- PART TWO ----" << endl;
+    cout << "Example: ";
+    solvePartTwo("exampleInput.txt");
+    cout << endl << "Input: ";
+    solvePartTwo("input.txt");
 }
 
 
@@ -46,4 +51,31 @@ void DayTwo::solvePartOne(string file) {
 }
 
 
-void DayTwo::solvePartTwo(string file);
+void DayTwo::solvePartTwo(string file) {
+
+    ifstream fin(PATH + file);
+
+    if(fin.fail()) { // throws unhandled exception if given invalid input
+        throw errc::no_such_file_or_directory;
+    }
+
+    int score = 0;
+    for(string line; getline(fin, line, '\n'); ) {
+
+        // convert chars to ints
+        int their = line.at(0) - 65;
+        char state = line.at(2);
+
+        if(state == 'Z') { // check for wins
+            score += ((their + 1) % 3) + 1; // increase score by value
+            score += 6;
+        } else if(state == 'Y') { // check for ties
+            score += their + 1;
+            score += 3;
+        } else { // must be loss
+            score += ((their + 2) % 3) + 1;
+        }
+    }
+
+    cout << score << endl;
+}
