f0fdcff7f336fc5e727be3c17ce4981a0127b416
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2022-12-03 17:45:39 -0700

    Completed Day Three, Part Two, and reoptimized Part 1 with vectors

--- Day03/DayThree.cpp
+++ Day03/DayThree.cpp
@@ -34,10 +34,7 @@ void DayThree::solvePartOne(string file) {
         string first = line.substr(0, line.size() / 2); // holds first half
         string last = line.substr(line.size() / 2); // holds last half
 
-        bool itemExists[52];
-        for(int i = 0; i < 52; i++) {
-            itemExists[i] = false;
-        }
+        vector<bool> itemExists(52);
 
         for(char c : first) {
             if(isupper(c)) { // uppercase
@@ -46,7 +43,7 @@ void DayThree::solvePartOne(string file) {
                 c -= 97;
             }
 
-            itemExists[c] = true;
+            itemExists.at(c) = true;
 
         }
 
@@ -59,7 +56,7 @@ void DayThree::solvePartOne(string file) {
                 c -= 97;
             }
 
-            if(itemExists[c]) {
+            if(itemExists.at(c)) {
                 sum += c + 1;
                 found = true;
             }
@@ -69,6 +66,52 @@ void DayThree::solvePartOne(string file) {
     cout << sum << endl;
 }
 
+
 void DayThree::solvePartTwo(string file) {
 
+    ifstream fin(PATH + file);
+
+    if(fin.fail()) { // throws unhandled exception if given invalid input
+        throw errc::no_such_file_or_directory;
+    }
+
+    int sum = 0;
+    for(vector<string> line(3); getline(fin, line.at(0), '\n') && getline(fin, line.at(1), '\n') && getline(fin, line.at(2), '\n'); ) {
+        vector<vector<bool>> elfHas(3, vector<bool> (52));
+
+        for(int i = 0; i < elfHas.size(); i++) {
+            for(char c : line.at(i)) {
+                if(isupper(c)) { // uppercase
+                    c -= 39;
+                } else { // lowercase
+                    c -= 97;
+                }
+
+                elfHas.at(i).at(c) = true;
+            }
+        }
+
+        for(int i = 0; i < elfHas.at(0).size(); i++) {
+            if(elfHas.at(0).at(i) && elfHas.at(1).at(i) && elfHas.at(2).at(i)) {
+                sum += i + 1;
+            }
+        }
+
+//        bool found = false;
+//        for(int i = 0; i < last.length() && !found; i++) {
+//            char c = last.at(i);
+//            if(isupper(c)) { // uppercase
+//                c -= 39;
+//            } else { // lowercase
+//                c -= 97;
+//            }
+//
+//            if(itemExists[c]) {
+//                sum += c + 1;
+//                found = true;
+//            }
+//        }
+    }
+
+    cout << sum << endl;
 }
--- Day03/DayThree.h
+++ Day03/DayThree.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 #ifndef ADVENTOFCODE2022_DAYTHREE_H
