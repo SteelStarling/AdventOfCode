bdef89e2f292eb570eb95825f334d0938f27f898
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-01-15 02:11:20 -0700

    Started creation of test system

---
+++ 2024/cpp/AOC24.hpp
@@ -0,0 +1,14 @@
+#ifndef AOC_24
+#define AOC_24
+
+// System based loosely on https://github.com/bcooperstl/advent-of-code-cpp-starter/
+
+#include "Solution24.hpp"
+
+class AdventOfCode_24 {
+    static const int daysInAdvent = 25;
+    
+    Solution_24 calendar[daysInAdvent];
+};
+
+#endif //AOC_24
---
+++ 2024/cpp/Solution24.hpp
@@ -0,0 +1,13 @@
+#ifndef SOLUTION_24
+#define SOLUTION_24
+
+#include <string>
+
+using namespace std;
+
+class Solution_24 {
+    virtual string partOneSolver(string);
+    virtual string partTwoSolver(string);
+};
+
+#endif //SOLUTION_24
