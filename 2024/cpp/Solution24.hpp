ea965830c8a1d8a0adbac9a5f76367e82968efd9
Taylor Hancock <58285541+SteelStarling@users.noreply.github.com>
2025-01-16 13:37:17 -0700

    Started work on testing system with Solution24.hpp file

--- 2024/cpp/Solution24.hpp
+++ 2024/cpp/Solution24.hpp
@@ -1,13 +1,46 @@
 #ifndef SOLUTION_24
 #define SOLUTION_24
 
+#include <iomanip>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 class Solution_24 {
-    virtual string partOneSolver(string);
-    virtual string partTwoSolver(string);
+
+public:
+
+    static const string exampleFile;
+    static const string inputFile;
+
+    Solution_24::Solution_24(int day, string filePath);
+
+    virtual string partOneSolver(string fileName);
+    virtual string partTwoSolver(string fileName);
+
+    int testDay() {
+        // Print Header
+        cout << "   =============== DAY " << setfill('0') << setw(2) << day << " ===============   " << endl;
+
+        // Get correct answers
+        
+
+        // Get answers from Part One
+        string exampleOneAnswer = partOneSolver(filePathPrefix + exampleFile);
+        string inputOneAnswer = partOneSolver(filePathPrefix + inputFile);
+
+        // Get answers from Part Two
+        string exampleTwoAnswer = partTwoSolver(filePathPrefix + exampleFile);
+        string inputTwoAnswer = partTwoSolver(filePathPrefix + inputFile);
+
+        // Print Footer
+        cout << "   =============== DAY " << setfill('0') << setw(2) << day << " ===============   " << endl;
+    }
+
+private:
+    const int day;
+    const string filePathPrefix; // stores path to files
 };
 
 #endif //SOLUTION_24
